a9b23c29ffb5dca2f4d1e9f6b2d7ed9195ce4e26
Chetan Chirag KH <chetanchirag24@gmail.com>
2021-07-18 13:07:08 +0530

    Add files via upload

---
+++ medreminderapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.medreminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView createnewAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createnewAccount=findViewById(R.id.createNewAccount);
+
+
+
+
+        createnewAccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ medreminderapp/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.medreminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView alreadyHaveaccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        alreadyHaveaccount=findViewById(R.id.alreadyHaveaccount);
+        alreadyHaveaccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
